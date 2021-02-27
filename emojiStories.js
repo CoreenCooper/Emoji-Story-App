0e508d151f6d6f36d4d21dc8a2f9bc3cc586d88e
Coreen Cooper <coreencooper@pursuit.org>
2021-02-27 14:17:59 -0500

    add corey files for review

--- emojiStories.js
+++ emojiStories.js
@@ -1,39 +1,56 @@
-const form = document.querySelector("form"); // grab form
-const inputNum = document.querySelector("#story-length-input"); // grab - user number 
-const p = document.querySelector("#emoji-story"); // grab - output destiantion
-const inputText = document.querySelector("#description-input");
-const h2 = document.querySelector("#history-title");
-const ul = document.querySelector("ul");
-const output = document.querySelector("ul");
-let emojisPicks = "";
-const generateBtn = document.querySelector("#generate-story-btn");
-const descriptionBtn = document.querySelector("#submit-description-btn");
-let count = 0;
+// generate emojis
+const storyForm = document.querySelector("#generate-story-form"); // grab form
+const storyLength = document.querySelector("#story-length-input"); // grab user input
+const emojiStory = document.querySelector("#emoji-story"); // display emojis
+let emojiPicks = "";
 
+// display description
+const descriptionForm = document.querySelector("#submit-description-form"); // grab button
+const descriptionInput = document.querySelector("#description-input"); // grab user input
+const ul = document.querySelector("ul"); // display list items
+const h2 = document.querySelector("#history-title"); // grab Saved Stories
+let n = 0; // update stories saved
 
-const updateStoryHistory = () => {
-  h2.textContent = `${count} Saved Stories`; // not certain why this value is doubling
-};
-
-const displayDescription = () => {
+const generateStory = () => {
+  const userTextInput = descriptionInput.value;
   const li = document.createElement("li");
-  li.textContent = `${emojisPicks} ${inputText.value} `;
-  ul.appendChild(li);
+  li.textContent = `${emojiPicks} ${userTextInput}`;
+  ul.appendChild(li); // how do you access the length of li's that have been created?
+  n++
+  h2.textContent = n === 1 ? `1 Saved Story` : `${n} Saved Stories`;
 };
 
-const randomEmojisSelector = (event) => {
-  const length = Number(inputNum.value); // why doesn't the variable work outside of the function?
-  event.preventDefault();
-  for (let i = 0; i < length; i++) {
-    const randomizeEmojis = Math.floor(Math.random() * (emojis.length - 1));
-    emojisPicks += emojis[randomizeEmojis];
+const generateEmojis = () => {
+  emojiPicks = "";
+  const userNumInput = Number(storyLength.value); // get input value
+  for (let i = 0; i < userNumInput; i++) {
+    const randomIdx = Math.floor(Math.random() * (emojis.length - 1));
+    emojiPicks += emojis[randomIdx];
+    storyLength.value = "";
   }
-  p.textContent = `Story: ${emojisPicks}`;
+  emojiStory.textContent = `Story: ${emojiPicks}`;
 };
 
-form.addEventListener("submit", (event) => {
-  randomEmojisSelector(event);
-  updateStoryHistory();
-  displayDescription();
-  count++;
+descriptionForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  generateStory();
 });
+
+storyForm .addEventListener("submit", (event) => {
+  event.preventDefault();
+  generateEmojis();
+});
+
+//// Vanessa - https://blog.jonnew.com/posts/poo-dot-length-equals-two
+
+// const arr = [2, 5, 4, 45, 32, 46, 78, 87, 98, 56, 23, 12];
+// const chooseRandom = (arr, num = 4) => {
+//    const res = [];
+//    for(let i = 0; i < num; i++){
+//       const random = Math.floor(Math.random() * arr.length);
+//       if(res.indexOf(arr[random]) !== res[i]){
+//       res.push(arr[random]);
+//    };
+// };
+// return res;
+// };
